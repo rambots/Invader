322582b99fbef10f29525b4c58bea72f3dc8fc7f
Mahim <mahim216@gmail.com>
2018-02-13 18:56:21 -0500

    Second Commit

--- 2018-Invader/src/org/usfirst/frc/team4571/robot/RobotJoystick.java
+++ 2018-Invader/src/org/usfirst/frc/team4571/robot/RobotJoystick.java
@@ -7,13 +7,16 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 public class RobotJoystick extends Joystick {
 	private Button 	button1,
 					button2,
-					button3;
+					button3,
+					button4; 
+
 
 	public RobotJoystick(int port) {
 		super(port);
 		this.button1 = new JoystickButton(this, 1);
 		this.button2 = new JoystickButton(this, 2);
 		this.button3 = new JoystickButton(this, 3);
+		this.button4 = new JoystickButton(this, 4);
 	}
 	
 	public double getY_Axis() {
@@ -22,5 +25,17 @@ public class RobotJoystick extends Joystick {
 	
 	public boolean isButton1Pressed() {
 		return this.button1.get();
+	}
+	
+	public boolean isButton2Pressed() {
+		return this.button2.get();
+	}
+	
+	public boolean isButton3Pressed() {
+		return this.button3.get();
+	}
+	
+	public boolean isButton4Pressed() {
+		return this.button4.get();
 	}
 }
