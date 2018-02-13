1fc0472ad746b87ef156d62caf637dde33d3a7cf
Mahim <mahim216@gmail.com>
2018-02-13 18:39:45 -0500

    First Commit

---
+++ 2018-Invader/src/org/usfirst/frc/team4571/robot/commands/TestDriveCommand.java
@@ -0,0 +1,48 @@
+package org.usfirst.frc.team4571.robot.commands;
+
+import org.usfirst.frc.team4571.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class TestDriveCommand extends Command {
+
+    public TestDriveCommand() {
+    	requires(Robot.DRIVE_SYSTEM);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	if(Robot.LEFT_JOYSTICK.isButton1Pressed()) {
+    		Robot.DRIVE_SYSTEM.teleOPDrive(0.5, 0.0);
+    	} else {
+    		Robot.DRIVE_SYSTEM.stop();
+    	}
+    	if(Robot.RIGHT_JOYSTICK.isButton1Pressed()) {
+    		Robot.DRIVE_SYSTEM.teleOPDrive(0.0, 0.3);
+    	} else {
+    		Robot.DRIVE_SYSTEM.stop();
+    	}
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.DRIVE_SYSTEM.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
---
+++ 2018-Invader/src/org/usfirst/frc/team4571/robot/subsystems/DriveSystem.java
@@ -0,0 +1,70 @@
+package org.usfirst.frc.team4571.robot.subsystems;
+
+import org.usfirst.frc.team4571.robot.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+/**
+ *
+ */
+public class DriveSystem extends Subsystem {
+	private WPI_TalonSRX 		topLeftMotor,
+								bottomLeftMotor,
+								topRightMotor,
+								bottomRightMotor;
+	private DifferentialDrive 	differentialDrive;
+	
+	public DriveSystem() {
+		this.topLeftMotor 		= new WPI_TalonSRX(RobotMap.TOP_LEFT_MOTOR);
+		this.bottomLeftMotor 	= new WPI_TalonSRX(RobotMap.BOTTOM_LEFT_MOTOR);
+		this.topRightMotor 		= new WPI_TalonSRX(RobotMap.TOP_RIGHT_MOTOR);
+		this.bottomRightMotor 	= new WPI_TalonSRX(RobotMap.BOTTOM_RIGHT_MOTOR);
+		
+		this.topLeftMotor.setSafetyEnabled(true);
+		this.bottomLeftMotor.setSafetyEnabled(true);
+		this.topRightMotor.setSafetyEnabled(true);
+		this.bottomRightMotor.setSafetyEnabled(true);
+		
+		topLeftMotor.setNeutralMode(NeutralMode.Brake);
+		bottomLeftMotor.setNeutralMode(NeutralMode.Brake);
+		topRightMotor.setNeutralMode(NeutralMode.Brake);
+		bottomRightMotor.setNeutralMode(NeutralMode.Brake);
+		
+		SpeedControllerGroup left 	= new SpeedControllerGroup(topLeftMotor, bottomLeftMotor);
+		SpeedControllerGroup right 	= new SpeedControllerGroup(topRightMotor, bottomRightMotor);
+		
+		this.differentialDrive = new DifferentialDrive(left, right);
+	}
+	
+	public void teleOPDrive(double left, double right) {
+		this.differentialDrive.tankDrive(left, right);
+	}
+	
+	public double getTopLeftMotorSpeed() {
+		return this.topLeftMotor.get();
+	}
+	
+	public double getBottomLeftMotorSpeed() {
+		return this.bottomLeftMotor.get();
+	}
+	
+	public double getTopRightMotorSpeed() {
+		return this.topRightMotor.get();
+	}
+	
+	public double getBottomRightMotorSpeed() {
+		return this.bottomRightMotor.get();
+	}
+	
+	public void stop() {
+		this.teleOPDrive(0.0, 0.0);
+	}
+
+    public void initDefaultCommand() {}
+}
+
