162455b02cd54e4f8e69b7c31c4e85a9ed00af12
Skyblade5694 <jiahuilimcsm@gmail.com>
2018-02-15 01:48:41 -0500

    AutoSelect

---
+++ src/org/usfirst/frc/team4571/robot/commands/auto/AutoSelect.java
@@ -0,0 +1,31 @@
+package org.usfirst.frc.team4571.robot.commands.auto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public final class AutoSelect {
+
+	protected static final String AUTONOMOUS_MODE = "Autonomous Mode";
+	
+	private Map<String, CommandGroup> modes;
+	
+	public AutoSelect(){
+		this.setModes(new HashMap<>());
+		modes = new HashMap<>();
+	    addAutoCommand("None", new CommandGroup());
+	}
+
+	public void addAutoCommand(String name, CommandGroup command) {
+		modes.put(name, command);
+	}
+
+	public Map<String, CommandGroup> getModes() {
+		return modes;
+	}
+
+	public void setModes(Map<String, CommandGroup> modes) {
+		this.modes = modes;
+	}
+}
